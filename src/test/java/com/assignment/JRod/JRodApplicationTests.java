cba9f02f8f9b29846f6829b67ae4547c4d5c49bb
JRod <jish0210@naver.com>
2020-10-27 16:52:18 +0900

    First commit

---
+++ src/test/java/com/assignment/JRod/JRodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.JRod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JRodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
